bdb78cf58e6ad0fca3d475602a59eeb0828490ba
dfnc1 <dianfnc06@gmail.com>
2026-04-09 05:51:24 +0700

    feat: add auth endpoint

---
+++ Expense-Tracker-API/app/routers/auth.py
@@ -0,0 +1,26 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app.schemas import Token, Register
+from app.databases import get_db
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+
+@router.post("/signup", response_model=Token)
+async def signup(payload: Register, conn= Depends(get_db)):
+    if not payload:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+    try:
+        pass
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+
+@router.post("/login", response_model=Token)
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], conn= Depends(get_db)):
+    user = None
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password", headers={"WWW-Authenticate": "Bearer"})
+    pass
