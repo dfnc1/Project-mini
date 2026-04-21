f05e78af286365b0f086d6b6c2014c70ac141f91
dfnc1 <dianfnc06@gmail.com>
2026-04-21 09:06:50 +0700

    feat: add users migration

---
+++ Expense-Tracker-API/app/migrations/versions/6d3d90ce93f2_create_user_table.py
@@ -0,0 +1,34 @@
+"""create user table
+
+Revision ID: 6d3d90ce93f2
+Revises: 
+Create Date: 2026-04-19 13:18:28.305267
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '6d3d90ce93f2'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        hashed_password VARCHAR(255) NOT NULL 
+    );
+    """)
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.execute("DROP TABLE users IF EXISTS")
