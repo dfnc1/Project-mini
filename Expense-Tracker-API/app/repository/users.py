2b1c172f4b13edf10f49a30c1b9a51a328b52456
dfnc1 <dianfnc06@gmail.com>
2026-04-21 12:14:01 +0700

    feat: add and rename email query

--- Expense-Tracker-API/app/repository/users.py
+++ Expense-Tracker-API/app/repository/users.py
@@ -2,9 +2,9 @@ import asyncpg
 
 from app.schemas import UserInDB, User
 
-async def add_user(user: UserInDB, conn: asyncpg.Pool) -> User:
-    return await conn.fetchrow("INSERT INTO users (username, hashed_password) VALUES ($1, $2) RETURNING username",
-                               user.username, user.hashed_password)
+async def add_user(payload: UserInDB, conn: asyncpg.Pool) -> User:
+    return await conn.fetchrow("INSERT INTO users (username, email, hashed_password) VALUES ($1, $2) RETURNING username",
+                               user.username, user.email, user.hashed_password)
 
-async def get_user(username: str, conn: asyncpg.Pool) -> User:
-    return await conn.fetchrow("SELECT * FROM users WHERE username = $1", username)
+async def get_user(email: str, conn: asyncpg.Pool) -> User:
+    return await conn.fetchrow("SELECT * FROM users WHERE email = $1", email)
