0840bb839dd01f18b83001159feba1c6c0376d18
dfnc1 <dianfnc06@gmail.com>
2026-04-24 22:27:00 +0700

    fix: resolve typo

--- Expense-Tracker-API/app/repository/users.py
+++ Expense-Tracker-API/app/repository/users.py
@@ -3,8 +3,8 @@ import asyncpg
 from app.schemas import UserInDB, User
 
 async def add_user(payload: UserInDB, conn: asyncpg.Pool) -> User:
-    return await conn.fetchrow("INSERT INTO users (username, email, hashed_password) VALUES ($1, $2) RETURNING username",
-                               user.username, user.email, user.hashed_password)
+    return await conn.fetchrow("INSERT INTO users (name, email, hashed_password) VALUES ($1, $2, $3) RETURNING name",
+                               payload.name, payload.email, payload.hashed_password)
 
 async def get_user(email: str, conn: asyncpg.Pool) -> User:
     return await conn.fetchrow("SELECT * FROM users WHERE email = $1", email)
