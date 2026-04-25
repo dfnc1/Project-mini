a0472133384f83d77eb0d919e764622667b3a213
dfnc1 <dianfnc06@gmail.com>
2026-04-25 14:35:06 +0700

    feat: add post query function

---
+++ Expense-Tracker-API/app/repository/posts.py
@@ -0,0 +1,14 @@
+import asyncpg
+
+from app.schemas import Post
+
+async def add_post(data: Post, conn: asyncpg.Pool) -> Post:
+    return await conn.fetchrow("INSERT INTO post (description, categories, amount) VALUES ($1, $2, $3)",
+                               data.description, data.category, data.amount)
+
+async def delete_post(id: int, conn: asyncpg.Pool) -> Post:
+    return await conn.fetchrow("DELETE FROM post WHERE id = $1", id)
+
+async def update_post(id: int, data: Post, conn: asyncpg.Pool) -> Post:
+    return await conn.fetchrow("UPDATE post SET description = $1, categories = $2, amount = $3 WHERE id = $4",
+                               data.description, data.category, data.amount, id)
